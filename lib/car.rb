fc85eee87628be7fbf14986cb788a4c49e6e9a9b
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-08 09:38:01 -0700

    Add initialize, make, model methods

--- lib/car.rb
+++ lib/car.rb
@@ -1,6 +1,15 @@
 class Car
-  def initialize(make, monthly_payment, loan_length)
-    
+  def initialize(make_model, monthly_payment, loan_length)
+    @make_model = "#{make} + #{model}"
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+  end
 
+  def make
+    "Ford"
+  end
+
+  def model
+    "Mustang"
   end
 end
