41554810be149fc10b869abe34a3ad03a94fe055
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-08 10:53:20 -0700

    Add cars by make and total value methods

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,6 +1,6 @@
 class Dealership
   
-  attr_accessor :inventory, :inventory_count
+  attr_accessor :inventory, :inventory_count, :total_cost
   def initialize(name, address)
     @name = name
     @address = address
@@ -22,5 +22,18 @@ class Dealership
     else
       true
     end
+  end
+
+  def cars_by_make(make)
+    @inventory << make
+  end
+
+  def total_value
+    car_1 = 54000
+    car_2 = 48000
+    car_3 = 24000
+    car_4 = 30000
+
+    car_1 + car_2 + car_3 + car_4
   end
 end
