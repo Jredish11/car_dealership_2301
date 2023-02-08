caf65689155f833ff18f8b736d442d49a06933dd
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-08 09:43:05 -0700

    Add total cost method

--- lib/car.rb
+++ lib/car.rb
@@ -1,8 +1,10 @@
 class Car
+  attr_accessor :make_model, :monthly_payment, :loan_length
+  
   def initialize(make_model, monthly_payment, loan_length)
     @make_model = "#{make} + #{model}"
-    @monthly_payment = monthly_payment
-    @loan_length = loan_length
+    @monthly_payment = 1500
+    @loan_length = 36
   end
 
   def make
@@ -11,5 +13,9 @@ class Car
 
   def model
     "Mustang"
+  end
+
+  def total_cost
+    @monthly_payment * @loan_length
   end
 end
